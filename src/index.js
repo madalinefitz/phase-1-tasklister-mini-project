3dfd248b8d19647d6bd8e214a4112852838d23d3
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-02-04 12:12:47 -0500

    extra practice

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () =>{
+    const form = document.getElementById("create-task-form")
+    form.addEventListener ('submit', function(e){
+        e.preventDefault()
+        createNewTask(e.target['new-task-description'].value)
+    })
+})
+
+function createNewTask(newTask){
+    const p = document.createElement ('p')
+    const btn = document.createElement('button')
+    btn.textContent = ' X '
+    p.textContent = newTask
+    document.getElementById('list').appendChild(p)
+    p.appendChild(btn)
+    btn.addEventListener('click', deleteTask)
+}
+
+function deleteTask(e){
+    e.target.parentNode.remove()
+}
